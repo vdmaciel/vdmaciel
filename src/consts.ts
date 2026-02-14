086c9f46b4b855f84b171bed9c79b9ee4a630efc
Victor Maciel <victormaciel0216@gmail.com>
2026-02-13 22:43:56 -0300

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,54 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'vdmaciel',
+  description: 'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'Victor Maciel',
+  locale: 'en-US',
+  featuredPostCount: 3,
+  postsPerPage: 10,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
+  {
+    href: '/projects',
+    label: 'projects',
+  },
+  {
+    href: '/about',
+    label: 'about',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/vdmaciel',
+    label: 'GitHub',
+  },
+  {
+    href: 'https://www.linkedin.com/in/vdmaciel',
+    label: 'LinkedIn',
+  },
+  {
+    href: 'mailto:vdmaciel.developer@gmail.com',
+    label: 'Email',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
